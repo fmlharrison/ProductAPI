7f99b49e58113864180f8130c4f6486de39f4534
Felix Harrison <felix.harrison@softwire.com>
2017-02-02 14:22:36 +0000

    Model and controller set up

---
+++ ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private Product[] products = new Product[]
+        {
+            new Product {Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1},
+            new Product {Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
+            new Product {Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M},
+            new Product {Id = 4, Name = "Gameboy DS", Category = "Electronics", Price = 99.99M}
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProducts(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
